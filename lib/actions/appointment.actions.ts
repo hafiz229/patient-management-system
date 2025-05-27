db0da52b7da66d0daf894fc6910c7a62f86eb340
A H M Golam Hafiz <hasnathafiz229@gmail.com>
2025-05-27 15:39:20 -0500

    SMS Notification added

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -98,6 +98,22 @@ export const getRecentAppointmentList = async () => {
   }
 };
 
+//  SEND SMS NOTIFICATION
+export const sendSMSNotification = async (userId: string, content: string) => {
+  try {
+    // https://appwrite.io/docs/references/1.5.x/server-nodejs/messaging#createSms
+    const message = await messaging.createSms(
+      ID.unique(),
+      content,
+      [],
+      [userId]
+    );
+    return parseStringify(message);
+  } catch (error) {
+    console.error("An error occurred while sending sms:", error);
+  }
+};
+
 //  UPDATE APPOINTMENT
 export const updateAppointment = async ({
   appointmentId,
@@ -116,7 +132,9 @@ export const updateAppointment = async ({
 
     if (!updatedAppointment) throw Error;
 
-    // TODO SMS Notification
+    const smsMessage = `Greetings from CarePulse. ${type === "schedule" ? `Your appointment is confirmed for ${formatDateTime(appointment.schedule!).dateTime} with Dr. ${appointment.primaryPhysician}` : `We regret to inform that your appointment for ${formatDateTime(appointment.schedule!).dateTime} is cancelled. Reason:  ${appointment.cancellationReason}`}.`;
+
+    await sendSMSNotification(userId, smsMessage);
 
     revalidatePath("/admin");
     return parseStringify(updatedAppointment);
